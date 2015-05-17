3ad29bbc43bbc376626ab7bcceed5c9820f1c10f
Connor Weng <foamtea30@gmail.com>
2015-05-17 16:25:46 +0800

    Modify delete_goods.sql.

--- script/delete_goods.sql
+++ script/delete_goods.sql
@@ -3,13 +3,18 @@ delimiter $$
 drop procedure delete_goods$$
 
 create procedure delete_goods(
-  in i_last_update int(10) unsigned
+  in i_last_update int(10) unsigned,
+  in i_min_store_id int(10) unsigned,
+  in i_max_store_id int(10) unsigned,
+  out o_count int
 )
 begin
   declare v_goods_id int(10) unsigned;
   declare goods_done int default false;
-  declare goods_cursor cursor for select goods_id from ecm_goods where last_update < i_last_update;
+  declare goods_cursor cursor for select goods_id from ecm_goods where store_id >= i_min_store_id and store_id < i_max_store_id and last_update < i_last_update;
   declare continue handler for not found set goods_done = true;
+
+  set o_count = 0;
 
   open goods_cursor;
 
@@ -18,6 +23,7 @@ begin
     if goods_done then
       leave goods_loop;
     end if;
+    set o_count = o_count + 1;
     delete from ecm_goods where goods_id = v_goods_id;
     delete from ecm_goods_spec where goods_id = v_goods_id;
     delete from ecm_goods_attr where goods_id = v_goods_id;
@@ -27,6 +33,8 @@ begin
   end loop;
 
   close goods_cursor;
+
+  select o_count;
 
 end$$
 
